76e64bf0fb322935fe90e78dae3200686c26480a
ErbolMamatov <erbolmamatov5@gmail.com>
2024-03-23 23:19:42 +0600

    first

---
+++ online1.py
@@ -0,0 +1,69 @@
+########################
+# a = 10
+# b = 20
+# print(a+b)
+###########СПИСОК#############
+# spisok = [1, 2, 3, 4, 5]
+# print(spisok[::-1])
+###############################
+# lord = ('erbol')
+# print(len(lord))
+###############################
+import random
+# my_list = list(range(20))
+# print(my_list)
+#
+# for a in my_list:
+#     if a % 2 == 0:
+#         print(a)
+
+
+
+
+# my_spisok = [10, 20, 30, 40, 50]
+# print(my_spisok[1:4])
+
+
+
+from random import randint
+
+# empy = []
+# for e in range(10):
+#     empy.append(random.randint(1 ,10))
+# empy.pop()
+# empy.insert(4,100)
+#
+# print
+
+
+
+
+#
+# erbol = ['a','b','c']
+# print(erbol)
+# erbol.clear()
+# print(erbol)
+
+
+
+
+# my_spisok = []
+# for e in range(10):
+#     my_spisok.append(random.randint(1,10))
+# print(my_spisok)
+# a = max(my_spisok)
+# print(a)
+
+
+
+
+
+# print(my_spisok)
+# my_spisok.sort()
+# print(my_spisok)
+# print(my_spisok[-1])
+
+
+
+
+
---
+++ online2.py
@@ -0,0 +1,75 @@
+#1
+# my_python = [10, 20, 30, 40, 50, 60, 70]
+# print(my_python[::-1])
+
+#2
+# alpha = ('Erbol')
+# beta = (' Mamatov')
+# print(alpha,beta)
+
+#3
+# color = ('banan, limon, apelsin')
+# print(len(color))
+
+#4
+
+# palindrom = ('madam')
+#
+# if palindrom in palindrom[::-1]:
+#     print('это слово является палиндромом')
+# else:
+#     print("это слово не является палиндромом")
+
+####################################################################################################################
+
+# a = float(input('ведите первое число '))
+# b = float(input('ведите второе число '))
+# if a < b:
+#     print(b)
+# elif b < a:
+#     print(a)
+# else:
+#     print("числа ровны")
+
+
+# a = float(input())
+# if 6 <= a <= 11:
+#     print('доброе утро')
+# elif 11<= a <= 18:
+#     print('добрый день')
+# elif 18 <= a <= 23:
+#     print('добрый вечер')
+# else:
+#     print('доброй ночи')
+
+
+
+# a = 10
+# if a > 0:
+#     print('True')
+# elif 0 > a:
+#     print('False')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
