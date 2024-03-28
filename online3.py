cbdaa9c18138f8f0735c7cf4665301d0dfe177a2
ErbolMamatov <erbolmamatov5@gmail.com>
2024-03-28 20:23:59 +0600

    hom4

---
+++ online3.py
@@ -0,0 +1,96 @@
+# a = float(input('ведите первое число '))
+# b = float(input('ведите второе число '))
+# if a < b:
+#     print(b)
+# elif b < a:
+#     print(a)
+# else:
+#     print("числа ровны")
+
+
+#                """ВЫВОДИТ ЧИСЛО КОТОРАЯ БОЛЬШЕ"""
+# a = float(input('ведите первое число '))
+# b = float(input('ведите второе число '))
+# if a < b:
+#     print(b)
+# elif b < a:
+#     print(a)
+# else:
+#     print("числа ровны")
+
+
+# a = float(input())
+# if 6 <= a <= 11:
+#     print('доброе утро')
+# elif 11<= a <= 18:
+#     print('добрый день')
+# elif 18 <= a <= 23:
+#     print('добрый вечер')
+# else:
+#     print('доброй ночи')
+
+########################################################################################################################
+
+
+
+
+
+# age = int(input())
+# if 0 <= age <= 18:
+#     print('подросток')
+# elif 18 <= age <= 25:
+#     print('взрослый')
+# else:
+#     print('дед')
+
+
+# number = int(input())
+# if number % 2 ==0:
+#     print(number * 2)
+# else:
+#     print('невозможно')
+
+
+
+#     print('больше пити букв')
+# else:
+#     print('меньше пити букв')
+
+
+# n = int(input())
+# for e in range(n+1):
+#     print(e)
+
+
+
+
+
+# k = int(input())
+# n = int(input())
+# # for e in range(k,n+1):
+# #     print(e)
+# summa = 0
+# for e in range(k, n + 1):
+#     summa += e
+# print(summa)
+
+
+
+# k = int(input())
+# n = int(input())
+# san = int(input())
+# for e in range(k,n+1):
+#     if san % 2==0:
+#         print("ww")
+#     print()
+
+
+
+
+
+
+
+
+
+
+
---
+++ online4.py
@@ -0,0 +1,18 @@
+# for e in range(0,10):
+#     print(e+1)
+
+
+# umn = int(input())
+# for i in range(umn * 10):
+#     print(i)
+
+
+# for e in range(5, 50, 5):
+#     print(e + 5)
+
+
+
+# slovo = 'python'
+# print(slovo[1])
+# print(slovo[3])
+# print(slovo[5])
