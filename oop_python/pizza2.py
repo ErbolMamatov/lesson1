d2de421a61ee07ce01e6b23e56de656d4358a8e5
ErbolMamatov <erbolmamatov5@gmail.com>
2024-05-10 19:20:14 +0600

    pizza_2

---
+++ oop_python/pizza2.py
@@ -0,0 +1,30 @@
+# from abc import ABC, abstractmethod
+#
+#
+# class DodoPizza(ABC):
+#     def __init__(self, sausage, cheese, oil, dough, mushroom, fried, check=1000):
+#         self.sausage = sausage
+#         self.cheese = cheese
+#         self.oil = oil
+#         self.dough = dough
+#         self.mushroom = mushroom
+#         self.fried = fried
+#         self.check = check
+#
+#     def peperoni(self):
+#         return f'пицца пеперони начинке {self.sausage} {self.cheese} {self.oil} {self.dough} цена - {self.check}'
+#
+#
+# class FriedPizza(DodoPizza):
+#
+#     def fried_pizza(self):
+#         return f'пицца жаренная начинка {self.sausage} {self.cheese} {self.oil} {self.dough} {self.mushroom} цена - {self.check}'
+#
+#
+# pizza = DodoPizza('колбоса', 'сыр', 'масло', 'тесто', 'грибы', 'жаренная', 100)
+# print(pizza.peperoni())
+#
+# pizza2 = FriedPizza('колбоса', 'сыр', 'масло', 'тесто', 'грибы', 'жаренная', 1090)
+# print(pizza2.fried_pizza())
+
+# АГАЙ МЕН ЖОЛДОН АЗЫР КЕЛДИМ БАТКЕНГЕ ОШОГО ЖАКШЫ ЖАСАЙ АЛГАН ЖОКМУН БУЛ КОДТУ ЖОЛДО ЖАСАГАМ
